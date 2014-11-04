be678ec2916470e333ba92159677634b5b728699
mmullerc <mmullerc@ucenfotec.ac.cr>
2014-11-04 08:27:42 -0800

    mas cabios en carpetas

--- AplicacionBecas/AplicacionGestionBecas/ModuloGestor.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports BLL
-Module ModuloGestor
-    Public objGestorCarrera As GestorCarrera = New GestorCarrera()
-    Public objGestorCurso As GestorCurso = New GestorCurso()
-    Public objGestorRequisito As GestorRequisito = New GestorRequisito()
-    Public objGestorRol As GestorRol = New GestorRol()
-    Public objGestorUsuario As GestorUsuario = New GestorUsuario()
-    Public objGestorBeneficio As New GestorBeneficios()
-End Module
